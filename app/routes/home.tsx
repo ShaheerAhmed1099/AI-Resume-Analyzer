b0e42bd0785d287dda1c80ae3f9832f296385dd6
Shaheer <shaheerahmed1099@gmail.com>
2026-01-19 18:59:59 +0500

    new file:   app/components/ATS.tsx 	new file:   app/components/Accordion.tsx 	new file:   app/components/Details.tsx 	new file:   app/components/ScoreBadge.tsx 	new file:   app/components/ScoreGuage.tsx 	new file:   app/components/Summary.tsx 	new file:   app/lib/pdf2img.ts 	modified:   app/lib/utils.ts 	modified:   app/routes.ts 	modified:   app/routes/home.tsx 	new file:   app/routes/resume.tsx 	modified:   app/routes/upload.tsx 	new file:   app/routes/wipe.tsx 	modified:   constants/index.ts

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "../../constants";
-import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
@@ -14,12 +14,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    const { auth } = usePuterStore();
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
 
     useEffect( () => {
         if(!auth.isAuthenticated) navigate('/auth?next=/');
     }, [auth.isAuthenticated])
+
+    useEffect(() => {
+      const loadResumes = async () => {
+        setLoadingResumes(true);
+
+        const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+        const parsedResumes = resumes?.map((resume) => (
+          JSON.parse(resume.value) as Resume
+        ));
+
+        setResumes(parsedResumes || []);
+        setLoadingResumes(false);
+      }
+
+      loadResumes();
+    },[]);
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
@@ -27,14 +46,32 @@ export default function Home() {
     <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track your Job Applications and Resume Ratings</h1>
-          <h2>Review your submissions and get AI-powered feedback</h2>
+          {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resume found. Upload your first resume to get feedback.</h2>
+          ) : (
+            <h2>Review your submissions and check feedback.</h2>
+          )}
         </div>
+        
+        {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+        )}
 
-      {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map(resume => (
             <ResumeCard key={resume.id} resume={resume} />
           ))}
+        </div>
+      )}
+
+      {!loadingResumes && resumes?.length === 0 && (
+        <div className="flex flex-col items-center justify-center mt-10 gap-4">
+          <Link to='/upload' className="primary-button w-fit text-xl font-semibold">
+            Upload Resume
+          </Link>
         </div>
       )}
     </section>
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -2,11 +2,16 @@ import React, { type FormEvent } from 'react';
 import { useState } from 'react';
 import Navbar from '~/components/Navbar';
 import FileUploader from '~/components/FileUploader';
-
-
+import { usePuterStore } from '~/lib/puter';
+import { useNavigate } from 'react-router';
+import { convertPdfToImage } from '~/lib/pdf2img';
+import { generateUUID } from "~/lib/utils";
+import { prepareInstructions } from '../../constants';
 
 const Upload = () => {
 
+    const { auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setIsStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null);
@@ -15,15 +20,64 @@ const Upload = () => {
         setFile(file);
     }
 
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file}: { companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+        setIsProcessing(true);
+
+        setIsStatusText('Uploading the file...');
+        const uploadedFile = await fs.upload([file]);
+
+        if(!uploadedFile) return setIsStatusText('Error: Failed to upload file');
+
+        setIsStatusText('Converting to Image...');
+        const imageFile = await convertPdfToImage(file);
+        if (!imageFile.file) return setIsStatusText('Error: Failed to convert PDF to image');
+
+        setIsStatusText('Uploading the image...');
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if(!uploadedImage) return setIsStatusText('Error: Failed to upload image');
+
+        setIsStatusText('Preparing Data...');
+        const uuid = generateUUID();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedback: '',
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setIsStatusText('Analyzing...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({ jobTitle, jobDescription })
+        )
+
+        if(!feedback) return setIsStatusText('Error: Failed to analyze resume');
+
+        const feedbackText = typeof feedback.message.content === 'string' ?
+        feedback.message.content : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setIsStatusText('Analysis complete, redirecting...');
+        navigate(`resume/${uuid}`);
+    };
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const form = e.currentTarget.closest('form');
+        const form = e.currentTarget.closest('form')
         if(!form) return;
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description');
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
+
+        if(!file) return;
+
+        handleAnalyze({ companyName, jobTitle, jobDescription, file});
     }
 
     return (
